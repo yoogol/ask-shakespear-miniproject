f1ab60f636dbb3b390f76febe3d347cba6b5c2f6
Yulia Shea <yulia.shea@gmail.com>
2016-03-08 16:53:22 -0500

    fixed some errors

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 window.onload = function() {
   var randomButton = document.querySelector('button');
   var inputBox = document.querySelector('input');
+  var youAsked = document.querySelector('.userAsked');
+  var poemLineEl = document.querySelector('.poem-line')
 
   randomButton.addEventListener('click',function(event) {
     event.preventDefault();
     var userTyped = inputBox.value;
+    youAsked.innerHTML = "";
+    poemLineEl.innerHTML = "";
     inputBox.value = "";
     var yourQuestion = document.createElement('div');
     yourQuestion.innerHTML = "YOU ASKED: " + userTyped;
-    var youAsked = document.querySelector('.userAsked');
     youAsked.appendChild(yourQuestion);
-    var poemLineEl = document.querySelector('.poem-line')
+
     var query = 'http://ShakeItSpeare.com/api/sentence';
     $.ajax({
       url: query,
